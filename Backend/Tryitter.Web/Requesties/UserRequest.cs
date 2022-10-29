18948da2cc11fb3cb785c60358b5342ac34b9cea
Caio Batista dos Santos <drcaiosan@gmail.com>
2022-10-29 16:30:54 -0300

    feat: request UserRequest

---
+++ Backend/Tryitter.Web/Requesties/UserRequest.cs
@@ -0,0 +1,9 @@
+namespace tryitter.Requesties;
+public class UserRequest
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public int Password { get; set; }
+    public string Module { get; set; }
+    public string Status { get; set; }
+}
