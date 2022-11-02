734389bca27bb654f8b3c83481d96462a7e11be3
Abner <abnerferreira84@gmail.com>
2022-11-02 15:47:36 -0300

    feat: add JsonIgnore nas colunas da classe User.

--- Backend/Tryitter.Web/Repository/TryitterRepository.cs
+++ Backend/Tryitter.Web/Repository/TryitterRepository.cs
@@ -47,7 +47,6 @@ public class UserRepository
     }
 
     public async Task<User> Create(User request) {
-        
         _context.Users.Add(request);
         await _context.SaveChangesAsync();
         return request;
