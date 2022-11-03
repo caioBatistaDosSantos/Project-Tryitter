5d4eeb270861e0d6fa060b546c6ddcbaa8baeef9
Abner <abnerferreira84@gmail.com>
2022-11-03 19:42:32 -0300

    feat: add o PostRepository no Program.cs, add dados na tabela Post.

--- Backend/Tryitter.Web/Context/TryitterContext.cs
+++ Backend/Tryitter.Web/Context/TryitterContext.cs
@@ -28,9 +28,6 @@ public class TryitterContext : DbContext, ITryitterContext
     {
         mb.Entity<User>()
             .HasKey(u => u.Id);
-
-        mb.Entity<Post>()
-            .HasKey(p => p.Content);
         
         mb.Entity<User>()
             .HasData(
@@ -65,6 +62,49 @@ public class TryitterContext : DbContext, ITryitterContext
                     Module = "Fundamentos do Desenvolvimento Web",
                     Status = "Ué, cadê o Perry?",
                     Password = "123456"
+                }
+            );
+
+        mb.Entity<Post>()
+            .HasKey(p => p.PostId);
+
+        mb.Entity<Post>()
+            .Property(p => p.Content)
+            .HasMaxLength(300);
+
+        mb.Entity<Post>()
+            .HasData(
+                new Post {
+                    PostId = 1,
+                    UserId = 2,
+                    UrlImage = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.contabeis.com.br%2Fartigos%2F7479%2Fbatman-e-suas-fraudes-contabeis%2F&psig=AOvVaw3lKYN8Pd65-zTxWCyQbFpH&ust=1667600202902000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCMj-xICFk_sCFQAAAAAdAAAAABAE",
+                    PublishedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    Content = "Combatendo o crime em Gothan por meio da programação.",
+                },
+                new Post {
+                    PostId = 2,
+                    UserId = 1,
+                    UrlImage = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.uol.com.br%2Fsplash%2Fnoticias%2F2021%2F08%2F04%2Fvilao-hulk.htm&psig=AOvVaw0_VAZo8OXL6wC9veOz7Zh9&ust=1667600642950000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCKjG9NGGk_sCFQAAAAAdAAAAABAE",
+                    PublishedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    Content = "Quebrando o computador porque não conseguiu centralizar o texto do botão!",
+                },
+                new Post {
+                    PostId = 3,
+                    UserId = 3,
+                    UrlImage = "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3DUT4COqFmN3U&psig=AOvVaw3lIWidWWZFj2XAimiVh8mQ&ust=1667600583332000&source=images&cd=vfe&ved=0CA0QjRxqFwoTCMCv0LWGk_sCFQAAAAAdAAAAABAE",
+                    PublishedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    Content = "Bwahahahahahaha",
+                },
+                new Post {
+                    PostId = 4,
+                    UserId = 4,
+                    UrlImage = "https://www.google.com/imgres?imgurl=https%3A%2F%2F64.media.tumblr.com%2F6a7a90ddf33fd8793cf0e9bcd0b3ae75%2F7325867cb61c486a-7a%2Fs500x750%2F57f4db17cfee6d364a68875f5d09bf4c4977f83a.png&imgrefurl=https%3A%2F%2Fericsrandomstuff.tumblr.com%2Fpost%2F45156076828%2F30-day-phineas-and-ferb-challenge-day-29&tbnid=oypBwrspvT3l_M&vet=12ahUKEwjP6_OdhpP7AhUeM7kGHS0JAR4QMygdegUIARCWAg..i&docid=9zBYnRKiD8KzzM&w=500&h=371&q=phineas%20and%20ferb%20where%27s%20perry&ved=2ahUKEwjP6_OdhpP7AhUeM7kGHS0JAR4QMygdegUIARCWAg",
+                    PublishedAt = DateTime.Now,
+                    UpdatedAt = DateTime.Now,
+                    Content = "Ué, cadê o Perry?",
                 }
             );
     }
--- Backend/Tryitter.Web/Models/Post.cs
+++ Backend/Tryitter.Web/Models/Post.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace tryitter.Models;
 public class Post
@@ -6,9 +7,9 @@ public class Post
     public int PostId { get; set; }
     public string? UrlImage { get; set; }
     public string? Content { get; set; }
-    // public int UserId { get; set; }
-    public User User { get; set; } = null!;
     public DateTime PublishedAt { get; set; } = DateTime.Now;
     public DateTime? UpdatedAt { get; set; }
+    public int UserId { get; set; }
+    public User User { get; set; } = null!;
 
 }
