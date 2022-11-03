4eb6c942e4e4e85988754adf31879820cc1b1998
Abner <abnerferreira84@gmail.com>
2022-11-03 18:47:31 -0300

    feat: add rota /post do verbo GET que retorna todos os posts.

--- Backend/Tryitter.Web/Controllers/PostController.cs
+++ Backend/Tryitter.Web/Controllers/PostController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using tryitter.PostRepository;
+using tryitter.Models;
 
 namespace Tryitter.Web.Controllers
 {
@@ -14,6 +15,16 @@ namespace Tryitter.Web.Controllers
         private PostRepository _repository { get; set; }
         public PostController(PostRepository repository) {
             _repository = repository;
+        }
+
+        /// <summary>
+        /// Lista os itens do objeto Post
+        /// </summary>
+        /// <returns>Os itens do objeto Post</returns>
+        /// <response code="200">Retorna os itens do objeto Post</response>
+        [HttpGet]
+        public async Task<ActionResult<List<Post>>> Get() {
+            return Ok(await _repository.Get());
         }
     }
 }
